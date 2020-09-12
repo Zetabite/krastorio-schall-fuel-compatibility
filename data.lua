b702f462187f3f89b890695a2923af2a3e162390
Adrian <zetabite.horzion@gmail.com>
2020-09-12 19:43:08 +0200

    Initial upload

---
+++ data.lua
@@ -0,0 +1,7 @@
+
+data.raw["locomotive"]["Schall-armoured-locomotive"].burner.fuel_category   = nil
+data.raw["locomotive"]["Schall-armoured-locomotive"].burner.fuel_categories = {"chemical", "vehicle-fuel"}
+data.raw["locomotive"]["Schall-armoured-locomotive-mk1"].burner.fuel_category   = nil
+data.raw["locomotive"]["Schall-armoured-locomotive-mk1"].burner.fuel_categories = {"chemical", "vehicle-fuel"}
+data.raw["locomotive"]["Schall-armoured-locomotive-mk2"].burner.fuel_category   = nil
+data.raw["locomotive"]["Schall-armoured-locomotive-mk2"].burner.fuel_categories = {"chemical", "vehicle-fuel"}
